12cb0d50b9ede9592a1d79b99459550364240f04
rarakukka <147248841+rarakukka@users.noreply.github.com>
2023-10-23 15:47:34 +0530

    Update update.py

--- update.py
+++ update.py
@@ -12,8 +12,8 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-UPSTREAM_REPO = 'https://github.com/rarakukka/Snfromdan'
-UPSTREAM_BRANCH = 'master'
+UPSTREAM_REPO = 'https://github.com/rarakukka/qzmlllll'
+UPSTREAM_BRANCH = 'main'
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
